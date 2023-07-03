6a587de23cd0c8b29b6c1fab35928e7855b264c1
AdamAugustinsky <AdamAugustinsky@gmail.com>
2023-07-03 18:00:23 -0300

    add fastify route

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,61 @@
-import { Client, create, ev } from "@open-wa/wa-automate";
+import { ChatId, Client, create, ev } from "@open-wa/wa-automate";
+import Fastify from "fastify";
 
 create().then((client) => start(client));
 
 ev.on("sessionData.**", async (sessionData, sessionId) => {
-  console.log(sessionId, sessionData);
+  console.log("session", sessionId, sessionData);
+});
+
+ev.on("sessionDataBase64.**", async (sessionDatastring, sessionId) => {
+  console.log("sessin base64", sessionId, sessionDatastring);
 });
 
 async function start(client: Client) {
-  const groups = await client.getAllGroups();
-
-  const groupId = groups.find(
-    (group) => group.name === "Teste envios automaticos"
-  )?.id;
-
-  if (groupId) {
-    const res = await client.sendText(groupId, "teste");
-
-    console.log("message sent", res);
-  }
-
-  client.onMessage(async (message) => {
-    if (message.body === "Hi") {
-      await client.sendText(message.from, "👋 Hello!");
-    }
+  const fastify = Fastify({
+    logger: true,
   });
+
+  fastify.get("/", (_, reply) => {
+    reply.send({ hello: "world" });
+  });
+
+  fastify.post(
+    "/send-message",
+    {
+      schema: {
+        body: {
+          type: "object",
+          properties: {
+            id: { type: "string" },
+            message: { type: "string" },
+          },
+          required: ["id", "message"],
+        },
+      },
+    },
+    async (request, reply) => {
+      const body = request.body as { id: ChatId; message: string };
+
+      const res = await client.sendText(body.id, body.message);
+
+      console.log("reply", {
+        status: true,
+        response: res,
+      });
+
+      reply.send({
+        status: true,
+        response: res,
+      });
+    }
+  );
+
+  fastify.listen(
+    { port: process.env.PORT ? Number(process.env.PORT) : 3000 },
+    (err, address) => {
+      if (err) throw err;
+      console.log(`Server is now listening on ${address}`);
+    }
+  );
 }
